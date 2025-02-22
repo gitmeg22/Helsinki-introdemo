2c8ddeb3e9b5fd8b74152fb9494213490ab60f2b
gitmeg22 <mcriley22@gmail.com>
2025-02-22 12:55:22 -0500

    Exercise 1

---
+++ introdemo/src/App.jsx
@@ -0,0 +1,43 @@
+// const App = () => {
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// // Refactor the code so that it consists of three new components: Header, Content, and Total. All data still resides in the App component, which passes the necessary data to each component using props. Header takes care of rendering the name of the course, Content renders the parts and their number of exercises and Total renders the total number of exercises.
+
+// // Define the new components in the file App.jsx.
+
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+// export default App
