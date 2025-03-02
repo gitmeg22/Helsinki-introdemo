0a08327c9a0aca18b4c877a22d1220445805caf2
gitmeg22 <mcriley22@gmail.com>
2025-03-02 11:21:30 -0500

    Add Part component

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -4,19 +4,14 @@
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content />
       <Total count={exercises1 + exercises2 + exercises3} />
     </div>
   )
@@ -30,10 +25,27 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Content = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
     <div>
-      <p>{props.part} {props.exercises}</p>
+      <Part partNumber={part1} exerciseCount={exercises1} />
+      <Part partNumber={part2} exerciseCount={exercises2} />
+      <Part partNumber={part3} exerciseCount={exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.partNumber} {props.exerciseCount}</p>
     </div>
   )
 }
