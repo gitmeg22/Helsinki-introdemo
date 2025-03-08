3b42b3566219ccd4f359236124545f1acebe4950
gitmeg22 <mcriley22@gmail.com>
2025-03-08 17:19:55 -0500

    Update variable arrays

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,37 +1,34 @@
-// Refactor the code so that it consists of three new components: Header, Content, and Total. All data still resides in the App component, which passes the necessary data to each component using props. Header takes care of rendering the name of the course, Content renders the parts and their number of exercises and Total renders the total number of exercises.
-
-// Define the new components in the file App.jsx.
-
 const App = () => {
   const course = 'Half Stack application development'
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  // parts[0].name; parts[0].exercises
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1.name} exercises1={part1.exercises} 
-        part2={part2.name} exercises2={part2.exercises}
-        part3={part3.name} exercises3={part3.exercises} 
-      />
-      <Total count={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total count={parts} />
+      {/* <Total count={parts[0].exercises + parts[1].exercises + parts[2].exercises} /> */}
     </div>
   )
 }
 
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -40,16 +37,18 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
-      <Part partNumber={props.part1} exerciseCount={props.exercises1} />
-      <Part partNumber={props.part2} exerciseCount={props.exercises2} />
-      <Part partNumber={props.part3} exerciseCount={props.exercises3} />
+      <Part partNumber={props.parts[0].name} exerciseCount={props.parts[0].exercises} />
+      <Part partNumber={props.parts[1].name} exerciseCount={props.parts[1].exercises} />
+      <Part partNumber={props.parts[2].name} exerciseCount={props.parts[2].exercises} />
     </div>
   )
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <div>
       <p>{props.partNumber} {props.exerciseCount}</p>
@@ -58,12 +57,22 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Number of exercises {props.count}</p>    
+      <p>Number of exercises {props.count[0].exercises + props.count[1].exercises + props.count[2].exercises}</p>
     </div>
   )
 }
+
+// const Total = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <p>Number of exercises {props.count}</p>
+//     </div>
+//   )
+// }
 
 
 export default App
